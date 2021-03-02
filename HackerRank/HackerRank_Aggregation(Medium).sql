2f3ed56443006dc4627b908641463b7282ca01c9
soojeong <qkrwl6@gmail.com>
2021-03-02 13:08:34 +0900

    upload

--- HackerRank/HackerRank_Aggregation(Medium).sql
+++ HackerRank/HackerRank_Aggregation(Medium).sql
@@ -11,10 +11,23 @@ SELECT ROUND(SQRT(POW(MAX(LAT_N)-MIN(LAT_N),2) + POW(MAX(LONG_W)-MIN(LONG_W),2))
 FROM STATION;
 
 /* Weather Observation Station 20 */
--- Oracle 사용
--- 1. 제공함수로 풀기
+-- 1. 제공함수로 풀기 - Oracle
 SELECT ROUND(MEDIAN(LAT_N),4)
 FROM STATION;
 
--- 2. 함수 사용하지 않고, 순위를 이용해 풀기
-
+-- 2. 함수 사용하지 않고, 순위 이용해 풀기 - MySQL
+SELECT ROUND(LAT_N,4)
+FROM (SELECT LAT_N,
+            ROW_NUMBER() OVER(ORDER BY LAT_N DESC) AS RNK
+     FROM STATION) T
+WHERE RNK = (SELECT CASE WHEN MAX(RNK)%2=0 THEN MAX(RNK)/2
+                    ELSE (MAX(RNK)+1)/2 END
+            FROM (SELECT LAT_N,
+                    ROW_NUMBER() OVER(ORDER BY LAT_N DESC) AS RNK
+                FROM STATION) T);
+                
+-- 3. 함수 사용하지 않고, 개수(like 순위)이용해 풀기 - MySQL
+SELECT ROUND(LAT_N, 4)
+FROM STATION S
+WHERE (SELECT COUNT(*) FROM STATION WHERE LAT_N < S.LAT_N)
+		= (SELECT COUNT(*) FROM STATION WHERE LAT_N > S.LAT_N);
