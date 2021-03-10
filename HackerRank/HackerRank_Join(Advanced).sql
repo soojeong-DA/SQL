6e1194bbdfd34ed77562aa18cc37ae11ed991fd7
soojeong <qkrwl6@gmail.com>
2021-03-10 14:20:55 +0900

    upload

--- HackerRank/HackerRank_Join(Advanced).sql
+++ HackerRank/HackerRank_Join(Advanced).sql
@@ -29,3 +29,31 @@ GROUP BY A.x, A.y
 HAVING COUNT(*) >= 2 OR A.x < A.y  -- 2개 이상, x가 y보다 작아야함
 ORDER BY A.x;
 
+/* Interviews */
+SELECT A.contest_id, 
+    A.hacker_id, 
+    A.name, 
+    SUM(E.total_submissions), 
+    SUM(E.total_accepted_submissions), 
+    SUM(D.total_views), 
+    SUM(D.total_unique_views)
+FROM Contests A INNER JOIN Colleges B ON A.contest_id = B.contest_id
+                 INNER JOIN Challenges C ON B.college_id = C.college_id
+                 -- View_Stats, Submission_Stats Table을 그대로 join하면, college의 challenge가 2번이상 개최된 경우, 중복값이 무더기로 생기는 문제 발생함
+                 -- 따라서 group by를 통해 수치들을 먼저 집약한 다음 join 해줘야함
+                 LEFT JOIN (SELECT challenge_id,
+                                SUM(total_views) AS total_views,
+                                SUM(total_unique_views) AS total_unique_views
+                            FROM View_Stats
+                           GROUP BY 1) D 
+                 ON C.challenge_id = D.challenge_id
+                 LEFT JOIN (SELECT challenge_id,
+                                SUM(total_submissions) AS total_submissions,
+                                SUM(total_accepted_submissions) AS total_accepted_submissions
+                            FROM Submission_Stats
+                           GROUP BY 1) E 
+                 ON C.challenge_id = E.challenge_id
+GROUP BY 1,2,3
+HAVING SUM(E.total_submissions) + SUM(E.total_accepted_submissions) + 
+    SUM(D.total_views) + SUM(D.total_unique_views) != 0
+ORDER BY 1;
--- SQL_recipe/6강_데이터가공_여러개의 값 조작.sql
+++ SQL_recipe/6강_데이터가공_여러개의 값 조작.sql
@@ -35,14 +35,129 @@ SELECT year,
 FROM quarterly_sales
 ORDER BY year;
 
-/* 3. 2개의 값 비율 계산 */
+-- 연간 평균 4분기 매출 계산
+SELECT year,
+		(COALESCE(q1,0) + COALESCE(q2,0) + COALESCE(q3,0) + COALESCE(q4,0)) 
+		/ (SIGN(COALESCE(q1,0)) + SIGN(COALESCE(q2,0)) + SIGN(COALESCE(q3,0)) + SIGN(COALESCE(q4,0)))
+		AS average
+FROM quarterly_sales
+ORDER BY year;
 
+/* 3. 2개의 값 비율 계산 */
+SELECT * FROM advertising_stats;
+
+-- CTR
+SELECT dt,
+		ad_id,
+		-- 정수 자료형 나누면, 정수 형태 출력됨 -> 자료형 변경한뒤 나눠줘야함
+		CAST(clicks AS float) / impressions AS CTR, -- float이 double precision보다 속도가 더 빠름
+		-- 실수를 상수 앞에 두고 계산하면, 암묵적으로 자료형 변환이 일어남
+		100.0 * clicks / impressions AS CTR_AS_PERCENT
+FROM advertising_stats
+WHERE dt = '2017-04-01';
+
+-- 0으로 나누는 것을 피해 CTR 계산
+-- -- 1. CASE식
+SELECT dt,
+		ad_id,
+		CASE 
+			WHEN impressions != 0 THEN CAST(clicks AS float) / impressions
+			ELSE NULL 
+		END AS CTR,
+		CASE 
+			WHEN impressions != 0 THEN 100.0 * clicks / impressions 
+			ELSE NULL 
+		END AS CTR_AS_PERCENT
+FROM advertising_stats;
+
+-- -- 2. NULL 전파 이용 - NULLIF
+SELECT dt,
+		ad_id,
+		CAST(clicks AS float) / NULLIF(impressions, 0) AS CTR, -- impressions가 0이면 NULL 반환
+		100.0 * clicks / NULLIF(impressions, 0) AS CTR_AS_PERCENT
+FROM advertising_stats;
 
 /* 4. 두 값의 거리 계산 */
+-- 절댓값, 제곱 평균 제곱근(RMS) 계산 <- 1차원
+SELECT abs(x1 - x2) AS abs,
+	sqrt(power(x1-x2,2)) AS rms
+FROM location_1d;
 
+-- xy 평면 위에 있는 두 점의 유클리드 거리 계산 <- 2차원
+SELECT sqrt(power(x1 - x2,2) + power(y1 - y2,2)) AS dist,
+	-- point 자료형과 거리 연상자(<->) 사용
+	point(x1, y1) <-> point(x2,y2) AS dist
+FROM location_2d;
 
 /* 5. 날짜/시간 계산 */
+-- 미래/과거 날짜/시간 계산
+SELECT user_id,
+		register_stamp::timestamp AS register_stamp,
+	-- interval 자료형 활용
+	register_stamp::timestamp + '1 hour'::interval AS after_1_hour,
+	register_stamp::timestamp - '30 minutes'::interval AS before_30_minutes,
+	-- interval 자료형 활용해서 계산 후, date 자료형으로 재변환
+	(register_stamp::date + '1 day'::interval)::date AS after_1_day,
+	(register_stamp::date + '1 month'::interval)::date AS before_1_month
+FROM mst_users_with_dates;
 
+-- 날짜간 차이 계산
+SELECT user_id,
+	CURRENT_DATE AS today,
+	register_stamp::date AS register_date,
+	CURRENT_DATE - register_stamp::date AS diff_days
+FROM mst_users_with_dates;
+
+-- 사용자 생년월일로 등록시점의 나이 계산(한국나이)
+SELECT EXTRACT(YEAR from register_stamp::date) - EXTRACT(YEAR from birth_date::date) + 1
+FROM mst_users_with_dates;
+
+-- 사용자 생년월일로 나이 계산(만나이) - age 함수 이용
+SELECT
+	-- 현재 기준 나이
+	EXTRACT(YEAR from age(birth_date::date)) AS current_age,
+	-- 등록시점의 나이
+	EXTRACT(YEAR from age(register_stamp::date, birth_date::date)) AS current_age
+FROM mst_users_with_dates;
+
+-- 날짜를 정수로 표현해서 나이 계산(날짜를 빼고 10000으로 나누면 계산됨)
+-- -- 미들웨어마다 날짜/시간 데이터 계산 표현에 차이가 커서 실수 많음
+-- -- 따라서, 수치/문자열 등으로 변환해서 다루는 것이 편함
+SELECT user_id,
+		substring(register_stamp,1,10) AS register_stamp,
+		birth_date,
+		-- 등록 시점의 나이
+		floor(
+			(CAST(replace(substring(register_stamp,1,10),'-','') AS integer)
+			- CAST(replace(birth_date,'-','') AS integer)
+			) / 10000 
+		) AS register_age,
+		-- 현재 시점의 나이
+		floor(
+			(CAST(replace(CURRENT_DATE::text,'-','') AS integer)
+			- CAST(replace(birth_date,'-','') AS integer)
+			) / 10000
+		) AS current_age
+FROM mst_users_with_dates;
 
 /* 6. IP 주소 다루기 */
+-- ip주소 다루기 위한 inet 자료형 활용
+-- IP 주소 비교(bool)
+SELECT CAST('127.0.0.1' AS inet) < CAST('127.0.0.2' AS inet) AS lt,
+	CAST('127.0.0.1' AS inet) > CAST('192.168.0.1' AS inet) AS gt
+;
+
+-- address/y 형식의 '네트워크 범위'에 해당 IP 주소가 포함되는지 판정 가능! (<< or >> 연산자 사용)
+SELECT CAST('127.0.0.1' AS inet) << CAST('127.0.0.0/8' AS inet) AS is_contained;
+
+-- IP 주소를 0으로 메우기(고정 길이 문자열)  <- inet 자료형 처럼 ip 주소 전용 자료형이 제공되지 않는 경우
+-- -- lpad함수로 문자열 왼쪽을 0으로 매워, 모든 10진수가 3자리 수가 되게하기
+SELECT ip,
+	lpad(split_part(ip, '.', 1), 3, '0') ||
+	lpad(split_part(ip, '.', 2), 3, '0') ||
+	lpad(split_part(ip, '.', 3), 3, '0') ||
+	lpad(split_part(ip, '.', 4), 3, '0') AS ip_padding
+FROM (SELECT CAST('192.168.0.1' AS text) AS ip) AS t;
+-- > 이렇게 맞춰 준 후, 대소 비교 등 가능!
+
 
