651881dbf1c8481b55f8c5943574afe4c5739db7
soojeong <qkrwl6@gmail.com>
2021-03-28 20:15:41 +0900

    modified

--- SQL_recipe/12강_시계열에 따른 사용자 전체의 상태 변화 찾기.sql
+++ SQL_recipe/12강_시계열에 따른 사용자 전체의 상태 변화 찾기.sql
@@ -41,3 +41,28 @@ SELECT year_month,
 FROM mst_user_with_year_month
 GROUP BY 1
 ORDER BY 1;
+
+/* 지속률과 정착률 산출하기 
+- 지속률: 등록일 기준으로 '이후 지정일 동안' 사용자가 서비스를 얼마나 이용했는지 나타내는 지표
+- 정착률: 등록일 기준으로 '이후 지정한 7일 동안' 사용자가 서비스를 사용했는지 나타내는 지표
+- 두 지표 모두 '사용자 수 / 등록 수'로 계산하지만, 집계 기간이 다름 */
+
+-- '로그 최근 일자'와 '사용자별 등록일'의 다음날 계산 
+WITH 
+action_log_with_mst_users AS (
+	SELECT u.user_id,
+		u.register_date,
+		-- action 날짜와 로그 전체의 최신 날짜를 자료형으로 변환
+		CAST(a.stamp AS date) AS action_date,
+		MAX(CAST(a.stamp  AS date)) OVER() AS laest_date,
+		-- 등록일 다음날의 날짜 계산
+		CAST(u.register_date::date + '1 day'::interval AS date) AS next_day_1
+	FROM mst_users u LEFT OUTER JOIN action_log a ON u.user_id = a.user_id
+)
+SELECT * 
+FROM action_log_with_mst_users
+ORDER BY register_date
+;
+
+
+
