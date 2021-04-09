2e5d53cc713d42222fe3fb512f02521252496345
soojeong <qkrwl6@gmail.com>
2021-04-09 23:46:38 +0900

    modified

--- SQL_recipe_for_data_analysis/12강_시계열에 따른 사용자 전체의 상태 변화 찾기.sql
+++ SQL_recipe_for_data_analysis/12강_시계열에 따른 사용자 전체의 상태 변화 찾기.sql
@@ -966,5 +966,72 @@ FROM monthly_users
 ORDER BY action_month
 ;
 
-
+-- 3. MAU 내역과 MAU 속성들의 '반복률' 계산
+WITH
+monthly_user_action AS (
+	-- 월별 사용자 액션 집약
+	SELECT DISTINCT u.user_id,
+		substring(u.register_date, 1, 7) AS register_month,
+		substring(a.stamp, 1, 7) AS action_month,
+		substring(CAST(a.stamp::date - '1 month'::interval AS text), 1, 7) AS action_month_priv
+	FROM mst_users u JOIN action_log a ON u.user_id = a.user_id
+),
+monthly_user_with_type AS (
+	-- 월별 사용자 분류 테이블
+	SELECT action_month,
+		user_id,
+		action_month_priv,
+		CASE
+			-- 1) 등록 월과 액션 월이 일치하면, 신규 사용자
+			WHEN register_month = action_month THEN 'new_user'
+			-- 2) 이전 월에 액션이 있다면, 리피트 사용자
+			WHEN action_month_priv = LAG(action_month) OVER(PARTITION BY user_id ORDER BY action_month) THEN 'repeat_user'
+			-- 3) 이외의 경우는 컴백 사용자
+			ELSE 'come_back_user'
+		END AS c
+	FROM monthly_user_action
+),
+monthly_users AS (
+	SELECT m1.action_month,
+		COUNT(m1.user_id) AS MAU,
+		COUNT(CASE WHEN m1.c = 'new_user' THEN 1 END) AS new_users,
+		COUNT(CASE WHEN m1.c = 'repeat_user' THEN 1 END) AS repeat_users,
+		COUNT(CASE WHEN m1.c = 'come_back_user' THEN 1 END) AS come_back_users,
+		-- ========================================================================
+		-- 1) 신규 리피트 사용자: 해당 월에 리피트 사용자이면서, 이전 달에 신규 사용자였던 사용자
+		-- 2) 기존 리피트 사용자: 해당 월에 리피트 사용자이면서, 이전 달에 리피트 사용자였던 사용자
+		-- 3) 컴백 리피트 사용자: 해당 월에 리피트 사용자이면서, 이전 달에 컴백 사용자였던 사용자
+		-- =========================================================================
+		COUNT(CASE WHEN m1.c = 'repeat_user' AND m0.c = 'new_user' THEN 1 END) AS new_repeat_users,
+		COUNT(CASE WHEN m1.c = 'repeat_user' AND m0.c = 'repeat_user' THEN 1 END) AS continuous_repeat_users,
+		COUNT(CASE WHEN m1.c = 'repeat_user' AND m0.c = 'come_back_user' THEN 1 END) AS come_back_repeat_users
+	FROM 
+		-- m1: 해당 월의 사용자 분류 테이블, m0: 이전 달의 사용자 분류 테이블
+		monthly_user_with_type m1 LEFT OUTER JOIN monthly_user_with_type m0 ON  m1.user_id = m0.user_id
+																		AND m1.action_month_priv = m0.action_month
+	GROUP BY 1
+)
+SELECT action_month,
+	mau,
+	new_users,
+	repeat_users,
+	come_back_users,
+	new_repeat_users,
+	continuous_repeat_users,
+	come_back_repeat_users,
+	-- 1) 이전 달에 신규 사용자이면서, 해당 월에 신규 리피트 사용자인 사용자의 비율
+	100.0 * new_repeat_users
+	/ NULLIF(LAG(new_users) OVER(ORDER BY action_month), 0)
+	AS priv_new_repeat_ratio,
+	-- 2) 이전 달에 리피트 사용자이면서, 해당 월에 기존 리피트 사용자인 사용자의 비율
+	100.0 * continuous_repeat_users
+	/ NULLIF(LAG(repeat_users) OVER(ORDER BY action_month), 0)
+	AS priv_continuous_repeat_ratio,
+	-- 3) 이전 달에 컴백 사용자이면서, 해당 월에 컴백 리피트 사용자인 사용자의 비율
+	100.0 * come_back_repeat_users
+	/ NULLIF(LAG(come_back_users) OVER(ORDER BY action_month), 0)
+	AS priv_come_back_repeat_ratio
+FROM monthly_users
+ORDER BY action_month
+;
 
