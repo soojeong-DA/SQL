20d7beb86f51fb0821d40c641a1fecc01955910b
soojeong <qkrwl6@gmail.com>
2021-04-10 23:41:53 +0900

    complete 12

--- SQL_recipe_for_data_analysis/12강_시계열에 따른 사용자 전체의 상태 변화 찾기.sql
+++ SQL_recipe_for_data_analysis/12강_시계열에 따른 사용자 전체의 상태 변화 찾기.sql
@@ -1035,3 +1035,139 @@ FROM monthly_users
 ORDER BY action_month
 ;
 
+/* 12-8. 방문 종류를 기반으로 '성장지수' 집계 ==========================================================
+- 성장지수: 사용자의 서비스 사용과 관련한 상태 변화를 수치화해서 서비스가 성장하는지 알려주는 지표
+- 성장지수가 1 이상이면 서비스가 성장한다는 뜻, 0보다 낮으면 서비스가 퇴보중이라는 뜻 
+- 성장시수 = Signup + Reactivation - Deactivation - Exit	 =======================================*/
+
+-- 1. 성장지수 산출을 위해 사용자 상태 집계(날짜별로 판정해야)
+WITH 
+unique_action_log AS (
+	-- 같은 날짜 로그를 중복해 세지 않도록, 중복 배제
+	SELECT DISTINCT user_id,
+		substring(stamp, 1, 10) AS action_date
+	FROM action_log
+),
+mst_calendar AS (
+	-- 집계하고 싶은 기간을 캘린더 테이블로 생성하기
+	SELECT CAST(generate_series('2016-10-01'::date, '2016-11-04'::date, '1 day'::interval)::date AS text) AS dt
+),
+target_date_with_user AS (
+	-- 사용자 마스터에 캘린더 테이블 날짜를 target_date로 추가
+	SELECT c.dt AS target_date,
+		u.user_id,
+		u.register_date,
+		u.withdraw_date
+	FROM mst_users u CROSS JOIN mst_calendar c
+),
+user_status_log AS (
+	SELECT u.target_date,
+		u.user_id,
+		u.register_date,
+		u.withdraw_date,
+		a.action_date,
+		-- 등록/탈퇴/액티브/비액티브로 사용자 상태구분
+		CASE WHEN u.register_date = a.action_date THEN 1 ELSE 0 END AS is_new,
+		CASE WHEN u.withdraw_date = a.action_date THEN 1 ELSE 0 END AS is_exit,
+		CASE WHEN u.target_date = a.action_date THEN 1 ELSE 0 END AS is_access,
+		LAG(CASE WHEN u.target_date = a.action_date THEN 1 ELSE 0 END)
+		OVER(PARTITION BY u.user_id ORDER BY u.target_date) AS was_access
+	FROM target_date_with_user u LEFT JOIN unique_action_log a ON u.user_id = a.user_id
+														AND u.target_date = a.action_date
+	WHERE
+		-- 집계 기간을 등록일 이후로만 필터링!
+		u.register_date <= u.target_date
+		-- 탈퇴 날짜가 포함되어 있으면, 집계 기간을 탈퇴 날짜 이전만으로 필터링!
+		AND (u.withdraw_date IS NULL OR u.target_date <= u.withdraw_date) 
+)
+SELECT target_date,
+	user_id,
+	is_new,
+	is_exit,
+	is_access,
+	was_access
+FROM user_status_log
+;
+
+-- 2. 매일의 성장지수 계산
+WITH 
+unique_action_log AS (
+	-- 같은 날짜 로그를 중복해 세지 않도록, 중복 배제
+	SELECT DISTINCT user_id,
+		substring(stamp, 1, 10) AS action_date
+	FROM action_log
+),
+mst_calendar AS (
+	-- 집계하고 싶은 기간을 캘린더 테이블로 생성하기
+	SELECT CAST(generate_series('2016-10-01'::date, '2016-11-04'::date, '1 day'::interval)::date AS text) AS dt
+),
+target_date_with_user AS (
+	-- 사용자 마스터에 캘린더 테이블 날짜를 target_date로 추가
+	SELECT c.dt AS target_date,
+		u.user_id,
+		u.register_date,
+		u.withdraw_date
+	FROM mst_users u CROSS JOIN mst_calendar c
+),
+user_status_log AS (
+	SELECT u.target_date,
+		u.user_id,
+		u.register_date,
+		u.withdraw_date,
+		a.action_date,
+		-- 등록/탈퇴/액티브/비액티브로 사용자 상태구분
+		CASE WHEN u.register_date = a.action_date THEN 1 ELSE 0 END AS is_new,
+		CASE WHEN u.withdraw_date = a.action_date THEN 1 ELSE 0 END AS is_exit,
+		CASE WHEN u.target_date = a.action_date THEN 1 ELSE 0 END AS is_access,
+		LAG(CASE WHEN u.target_date = a.action_date THEN 1 ELSE 0 END)
+		OVER(PARTITION BY u.user_id ORDER BY u.target_date) AS was_access
+	FROM target_date_with_user u LEFT JOIN unique_action_log a ON u.user_id = a.user_id
+														AND u.target_date = a.action_date
+	WHERE
+		-- 집계 기간을 등록일 이후로만 필터링!
+		u.register_date <= u.target_date
+		-- 탈퇴 날짜가 포함되어 있으면, 집계 기간을 탈퇴 날짜 이전만으로 필터링!
+		AND (u.withdraw_date IS NULL OR u.target_date <= u.withdraw_date) 
+),
+user_growth_index AS (
+	SELECT *,
+		CASE
+			-- 어떤 날짜에 신규 등록 OR 탈퇴한 경우, signup or exit으로 판정
+			WHEN is_new + is_exit = 1 THEN
+				CASE 
+					WHEN is_new = 1 THEN 'signup'
+					WHEN is_exit = 1 THEN 'exit'
+				END
+			-- 신규 등록과 탈퇴가 아닌 경우, reactivation or deactivation으로 판정
+			-- 만약 reactivation, deactivation의 정의에 맞이 않는 경우는 NULL로 지정
+			WHEN is_new + is_exit = 0 THEN
+				CASE
+					WHEN was_access = 0 AND is_access = 1 THEN 'reactivation'
+					WHEN was_access = 1 AND is_access = 0 THEN 'deactivation'
+				END
+			-- 어떤 날짜에 신규 등록과 탈퇴를 함께 했다면(is_new + is_exit = 2) NULL로 지정
+		END AS growth_index
+	FROM user_status_log
+)
+SELECT target_date,
+	SUM(CASE growth_index WHEN 'signup' THEN 1 ELSE 0 END) AS signup,
+	SUM(CASE growth_index WHEN 'reactivation' THEN 1 ELSE 0 END) AS reactivation,
+	SUM(CASE growth_index WHEN 'deactivation' THEN 1 ELSE 0 END) AS deactivation,
+	SUM(CASE growth_index WHEN 'exit' THEN 1 ELSE 0 END) AS exit,
+	-- 성장지수 계산!(4개 모두 더하면 됨. 단, deactivation, exit는 음수로 변환해서)
+	SUM(
+		CASE growth_index
+			WHEN 'signup' THEN 1
+			WHEN 'reactivation' THEN 1
+			WHEN 'deactivation' THEN -1
+			WHEN 'exit' THEN -1
+			ELSE 0
+		END
+	) AS growth_index
+FROM user_growth_index
+GROUP BY target_date
+ORDER BY target_date
+;
+
+
+
