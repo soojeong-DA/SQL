08063e3bc440231a62ef79a370d0c7c2785ff23b
soojeong <qkrwl6@gmail.com>
2021-05-07 14:38:38 +0900

    upload

--- SQL_recipe_for_data_analysis/19강_데이터 중복 검출.sql
+++ SQL_recipe_for_data_analysis/19강_데이터 중복 검출.sql
@@ -31,5 +31,84 @@ FROM mst_categories_with_key_num
 WHERE key_num > 1 -- ID가 중복되는 경우
 ;
 
+/* 19-2. 로그 중복 검출하기 
+- 사용자가 버튼을 2회 연속으로 클릭하거나 페이지의 새로고침으로 인해 로그가 2회 동시 발생하는 경우 등 =============================== */
 
+-- 1. 중복 데이터 확인
+SELECT user_id,
+	products,
+	-- 데이터를 배열로 집약하고, 쉼표로 구분된 문자열로 변환
+	string_agg(session, ',') AS session_list,
+	string_agg(stamp,',') AS stamp_list
+FROM dup_action_log
+GROUP BY user_id, products
+HAVING COUNT(*) > 1
+;
 
+-- 2. 중복 데이터 배제하기 (같은 session, user_id, action, proudcts일 경우)
+-- 2-1. timestamp가 가장 오래된 데이터만 남기는 방법 (집약함수 사용)
+SELECT session,
+	user_id,
+	action,
+	products,
+	MIN(stamp)
+FROM dup_action_log
+GROUP BY session, user_id, action, products
+;
+
+-- 2-2. ROW_NUMBER를 사용해 중복을 배제하는 방법
+WITH
+dup_action_log_with_order_num AS (
+	SELECT *,
+		-- 중복된 데이터에 순번 붙이기
+		ROW_NUMBER() OVER(PARTITION BY session, user_id, action, products ORDER BY stamp) AS order_num
+	FROM dup_action_log
+)
+SELECT session,
+	user_id,
+	action,
+	products,
+	stamp
+FROM dup_action_log_with_order_num
+WHERE order_num = 1  -- 순번이 1인 데이터(중복된 것 중에서 가장 앞의 것)만
+;
+
+-- 3. timestamp의 간격을 확인해, 일정 시간 이내의 로그를 중복으로 취급하는 방법 (session id 등을 사용할 수 없는 경우)
+-- 3-1. 같은 user_id, action, products 조합에 대해, 이전 action으로부터의 경과 시간을 계산
+WITH
+dup_action_log_with_lag_seconds AS (
+	SELECT user_id,
+		action,
+		products,
+		stamp,
+		-- 같은 사용자와 상품 조합에 대한 이전 액션으로부터의 경과 시간(초단위) 계산
+		EXTRACT(epoch from stamp::timestamp - LAG(stamp::timestamp) OVER(PARTITION BY user_id, action, products
+																		ORDER BY stamp)) AS lag_seconds
+	FROM dup_action_log
+)
+SELECT *
+FROM dup_action_log_with_lag_seconds
+ORDER BY stamp
+;
+
+-- 3-2. 30분 이내의 같은 액션은 중복으로 보고 배제
+WITH
+dup_action_log_with_lag_seconds AS (
+	SELECT user_id,
+		action,
+		products,
+		stamp,
+		-- 같은 사용자와 상품 조합에 대한 이전 액션으로부터의 경과 시간(초단위) 계산
+		EXTRACT(epoch from stamp::timestamp - LAG(stamp::timestamp) OVER(PARTITION BY user_id, action, products
+																		ORDER BY stamp)) AS lag_seconds
+	FROM dup_action_log
+)
+SELECT user_id,
+	action,
+	products,
+	stamp
+FROM dup_action_log_with_lag_seconds
+WHERE lag_seconds IS NULL 
+OR lag_seconds >= 30*60
+ORDER BY stamp
+;
