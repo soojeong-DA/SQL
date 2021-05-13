9f435f128597e7621526239d15e28fb8d921a785
soojeong <qkrwl6@gmail.com>
2021-05-13 21:02:26 +0900

    21-3

--- SQL_recipe_for_data_analysis/21강_검색 기능 평가.sql
+++ SQL_recipe_for_data_analysis/21강_검색 기능 평가.sql
@@ -97,3 +97,93 @@ AND next_action = 'search'
 GROUP BY keyword, result_num, next_keyword, next_result_num
 ORDER BY retry_count DESC
 ;
+
+/* 19-3. 재검색 키워드를 분류해서 집계 
+- 어떤 상태와 동기로 재검색을 했는지 사용자의 패턴에 따라 분석해보자 ========================================================== */
+
+-- 1. NoMatch에서의 조건 변경한 경우
+--> 해당 재검색 키워드는 동의어 사전과 사용자 사전에 추가할 키워드 후보가 됨
+-- NoMatch에서 재검색 키워드 집계
+WITH
+access_log_with_next_search AS (
+	SELECT stamp,
+		session,
+		action,
+		keyword,
+		result_num,
+		-- 다음 행의 액션, 키워드, 검색 결과 수 추출
+		LEAD(action) OVER(PARTITION BY session ORDER BY stamp ASC) AS next_action,
+		LEAD(keyword) OVER(PARTITION BY session ORDER BY stamp ASC) AS next_keyword,
+		LEAD(result_num) OVER(PARTITION BY session ORDER BY stamp ASC) AS next_result_num
+	FROM access_log
+)
+SELECT keyword,
+	result_num,
+	COUNT(*) AS retry_count,
+	next_keyword,
+	next_result_num
+FROM access_log_with_next_search
+WHERE action = 'search'
+AND next_action = 'search'
+AND result_num = 0
+GROUP BY keyword, result_num, next_keyword, next_result_num
+;
+
+-- 2. 검색 결과 필터링한 경우
+--> 재검색한 검색 키워드가 원래의 검색 키워드를 포함하고 있는 경우, 검색을 조금 더 필터링하고 싶다는 의미
+--> 자주 사용되는 필터링 키워드를 연관 검색어 등으로 출력해서, 사용자가 원하는 콘텐츠를 빠르게 찾을 수 있게 해야
+-- 검색 결과 필터링 시의 재검색 키워드  집계
+WITH
+access_log_with_next_search AS (
+	SELECT stamp,
+		session,
+		action,
+		keyword,
+		result_num,
+		-- 다음 행의 액션, 키워드, 검색 결과 수 추출
+		LEAD(action) OVER(PARTITION BY session ORDER BY stamp ASC) AS next_action,
+		LEAD(keyword) OVER(PARTITION BY session ORDER BY stamp ASC) AS next_keyword,
+		LEAD(result_num) OVER(PARTITION BY session ORDER BY stamp ASC) AS next_result_num
+	FROM access_log
+)
+SELECT keyword,
+	result_num,
+	COUNT(*) AS retry_count,
+	next_keyword,
+	next_result_num
+FROM access_log_with_next_search
+WHERE action = 'search'
+AND next_action = 'search'
+AND next_keyword LIKE CONCAT('%', keyword, '%')   -- 다음 검색 키워드가 기존 검색 키워드를 포함하고 있는 경우만
+GROUP BY keyword, result_num, next_keyword, next_result_num
+;
+
+-- 3. 검색 키워드를 변경한 경우
+--> 완전히 다른 검색 키워드를 사용해 재검색을 한 경우, 기존 검색 키워드를 사용한 검색 결과에 원하는 내용이 없다는 의미
+--> 동의어 사전이 제대로 기능하지 못했거나 다른 이유 등 어떤 이유로 행했는지 집계해보자
+-- 검색 키워드 변경한 경우의 재검색 집계
+WITH
+access_log_with_next_search AS (
+	SELECT stamp,
+		session,
+		action,
+		keyword,
+		result_num,
+		-- 다음 행의 액션, 키워드, 검색 결과 수 추출
+		LEAD(action) OVER(PARTITION BY session ORDER BY stamp ASC) AS next_action,
+		LEAD(keyword) OVER(PARTITION BY session ORDER BY stamp ASC) AS next_keyword,
+		LEAD(result_num) OVER(PARTITION BY session ORDER BY stamp ASC) AS next_result_num
+	FROM access_log
+)
+SELECT keyword,
+	result_num,
+	COUNT(*) AS retry_count,
+	next_keyword,
+	next_result_num
+FROM access_log_with_next_search
+WHERE action = 'search'
+AND next_action = 'search'
+AND next_keyword NOT LIKE CONCAT('%', keyword, '%')   -- 다음 검색 키워드가 기존 검색 키워드를 포함하지 않은 경우만
+GROUP BY keyword, result_num, next_keyword, next_result_num
+;
+
